f14ba45765aafbe34647b89505b3e47bde2cb559
KeyziTheOne <keyzimail@gmail.com>
2021-05-04 23:55:57 +0400

    table click action

--- src/sample/operator/operator.java
+++ src/sample/operator/operator.java
@@ -1,12 +1,14 @@
 package sample.operator;
 
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableView;
 
 import java.sql.SQLException;
 
+import javafx.scene.input.MouseEvent;
 import sample.Config;
 import sample.store.Store;
 
@@ -23,6 +25,15 @@ public class operator extends Config {
         //TABLE VIEW AND DATA
         Store store = Store.getStore();
         store.loadTableData(carOwnerTable,"SELECT * FROM car_owner");
+        carOwnerTable.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+//                if (event.isPrimaryButtonDown() && event.getClickCount() == 2) {
+                if (event.isPrimaryButtonDown()) {
+                    System.out.println(carOwnerTable.getSelectionModel().getSelectedItem());
+                }
+            }
+        });
     }
 
 }
