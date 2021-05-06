97062692880113f2069b83072c971d07bd0d0bad
KeyziTheOne <keyzimail@gmail.com>
2021-05-07 00:40:05 +0400

    шаблон страницы задач

---
+++ src/sample/models/DBComboBox.java
@@ -0,0 +1,21 @@
+package sample.models;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+import sample.store.Store;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBComboBox extends ComboBox<ComboItem> {
+
+    public void fillFromBD(Store store, String SQL) throws SQLException, ClassNotFoundException {
+        ObservableList<ComboItem> list = FXCollections.observableArrayList();
+        ResultSet rs = store.execQuery(SQL);
+        while (rs.next()) {
+            list.add(ComboItem.getItemFromResultSet(rs));
+        }
+        this.setItems(list);
+    }
+}
